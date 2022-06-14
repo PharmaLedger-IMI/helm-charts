0d76233725636c41c39a33582c53564840a50566
Torsten Gippert <46532698+tgip-work@users.noreply.github.com>
2022-06-14 11:18:07 +0200

    remove bayer module

--- charts/ethadapter/docs/secret_provider_class/modules/aws_iam_role_for_service_account/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "this_role_arn" {
-  value       = aws_iam_role.main.arn
-  description = "The ARN of the IAM role"
-}
-output "this_role_name" {
-  value       = aws_iam_role.main.name
-  description = "The name of the IAM role"
-}
